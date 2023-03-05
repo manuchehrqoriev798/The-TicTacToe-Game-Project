7726905608a01ff216de86d2dfb3c6622f91de22
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-03-05 22:05:56 +0600

    description and game over popup problems solved

--- tictactoe.js
+++ tictactoe.js
@@ -26,26 +26,32 @@ document.querySelector('#close-link').addEventListener('click', function(e){
 
 
 // HTML Elements
-const statusDiv = document.querySelector('.status');          // interacting with elements from HTML through DOM      
-const resetDiv = document.querySelector('.reset');            // interacting with elements from HTML through DOM    
-const cellDivs = document.querySelectorAll('.game-cell');     // interacting with elements from HTML through DOM         
+const statusDiv = document.querySelector('.status');               
+const resetDiv = document.querySelector('.reset');               
+const cellDivs = document.querySelectorAll('.game-cell');             
 const popupTitle = document.querySelector('.popup-title')
+const popupReset = document.querySelector('.popup-reset')
 
 // game constants
-const xSymbol = '×';             // special signs of constants of the game
-const oSymbol = '○';             // special signs of constants of the game
+const xSymbol = '×';          
+const oSymbol = '○';          
 
 // game variables
-let gameIsLive = true;           // if gameIsLive becomes false game finishes
-let xIsNext = true;              // if xIsNext false it would be turn of o
+let gameIsLive = true;           
+let xIsNext = true;           
 
 
 // functions
-const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;   // ternary statement: if there is no x print xSymbol; if there is print oSymbol
+const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;   
+function handleGameEnd() {
+  if (!gameIsLive) {
+    document.querySelector('.game-over').style.display = 'block'
+  }
+}
 
 
 // handleWin(letter).style.color = 'green'
-const handleWin = (letter) => {                                          // function that shows text to notify the winner
+const handleWin = (letter) => {                                          
   gameIsLive = false;
   if (letter === 'x') {
     statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
@@ -56,11 +62,13 @@ const handleWin = (letter) => {                                          // func
   } else {
     statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
   }
+  setTimeout(handleGameEnd, 1000);
+  popupTitle.innerHTML = `Game finished,  ${letterToSymbol(letter)} has won!`;
   }
 
 
 
-const checkGameStatus = () => {                                                           // function of listening all winning combination
+const checkGameStatus = () => {                                                           
   const topLeft = cellDivs[0].classList[1];
   const topMiddle = cellDivs[1].classList[1];
   const topRight = cellDivs[2].classList[1];
@@ -134,7 +142,7 @@ const checkGameStatus = () => {                                                 
 
 
 // event Handlers
-const handleReset = () => {                                             // reset bottom: remove x, 0, won. Bring text of xSymbol is next. Change style of status (text)
+const handleReset = () => {                                             
   xIsNext = true;
   statusDiv.innerHTML = `${xSymbol} now is your turn`;   
   statusDiv.style.color = 'white'
@@ -145,16 +153,35 @@ const handleReset = () => {                                             // reset
     cellDiv.classList.remove('won');
   }
   gameIsLive = true;
-};
+}
+const handlePopupReset = () =>{                                       
+  xIsNext = true
+  statusDiv.innerHTML = `${xSymbol} now is your turn`;   
+  statusDiv.style.color = 'white'
+  statusDiv.style.borderLeft = '4px solid white';
+  for (const cellDiv of cellDivs) {
+    cellDiv.classList.remove('x');
+    cellDiv.classList.remove('o');
+    cellDiv.classList.remove('won');
+  }
+  gameIsLive = true;
+  document.querySelector('.game-over').style.display = 'none'
+}
 
-const handleCellClick = (e) => {                                       // function that adds x or o to as 2nd class of grid-cells.
+
+
+
+
+
+
+const handleCellClick = (e) => {                                       
   const classList = e.target.classList;
 
-  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {   // x or o. Not both or multiple
+  if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {   
     return;
   }
 
-  if (xIsNext) {                                                       // first adds x then o then x then o. 
+  if (xIsNext) {                                                       
     classList.add('x');
     checkGameStatus();
   } else {
@@ -165,24 +192,221 @@ const handleCellClick = (e) => {                                       // functi
 
 
 // event listeners
-resetDiv.addEventListener('click', handleReset);                     // while clicking to resetDiv it declares handleReset function
+resetDiv.addEventListener('click', handleReset);  
+popupReset.addEventListener('click', handlePopupReset)                  
 
-for (const cellDiv of cellDivs) {                                    // while clicking in grid-cells that are children of grid it declares handleCellClick which adds x or o to element as addition class
+for (const cellDiv of cellDivs) {                                   
   cellDiv.addEventListener('click', handleCellClick)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // window.addEventListener('load', function(){
 //   setTimeout(
 //     function () {
-//       document.querySelector('.game-over').style.display = 'block'
-//       popupTitle.innerHTML = `Game finished ! ${letterToSymbol(letter)} has won!`;
+//       document.querySelector('.description').style.display = 'block'
 //     },
-//     200
+//     500
 //   )
 //   });
 
-// document.querySelector('.reset').addEventListener('click', function(){
-//   document.querySelector('.game-over').style.display = 'none'
+// document.querySelector('#close-button').addEventListener('click', function(){
+//   document.querySelector('.description').style.display = 'none'
 // })
+// document.querySelector('#close-link').addEventListener('click', function(e){
+//   e.preventDefault(); // this is for href atribute of a in html. Without it href element would refresh page 
+//   document.querySelector('.description').style.display = 'none'
+// })
+
+
+
+
+
+
+
+// // HTML Elements
+// const statusDiv = document.querySelector('.status');          // interacting with elements from HTML through DOM      
+// const resetDiv = document.querySelector('.reset');            // interacting with elements from HTML through DOM    
+// const cellDivs = document.querySelectorAll('.game-cell');     // interacting with elements from HTML through DOM         
+// const popupTitle = document.querySelector('.popup-title')
+// const popupReset = document.querySelector('popup-reset')
+
+// // game constants
+// const xSymbol = '×';             // special signs of constants of the game
+// const oSymbol = '○';             // special signs of constants of the game
+
+// // game variables
+// let gameIsLive = true;           // if gameIsLive becomes false game finishes
+// let xIsNext = true;              // if xIsNext false it would be turn of o
+
+
+// // functions
+// const letterToSymbol = (letter) => letter === 'x' ? xSymbol : oSymbol;   // ternary statement: if there is no x print xSymbol; if there is print oSymbol
+// function handleGameEnd() {
+//   if (!gameIsLive) {
+//     document.querySelector('.game-over').style.display = 'block'
+//   }
+// }
+
+// // handleWin(letter).style.color = 'green'
+// const handleWin = (letter) => {                                          // function that shows text to notify the winner
+//   gameIsLive = false;
+//   if (letter === 'x') {
+//     statusDiv.innerHTML = `${letterToSymbol(letter)} has won!`;
+//     statusDiv.style.borderLeft = '4px solid rgb(0, 255, 8)';
+//     statusDiv.style.backgroundColor = 'black'
+//     statusDiv.style.color = 'rgb(0, 255, 8)'
+//     statusDiv.style.transition = '0.5s'
+//   } else {
+//     statusDiv.innerHTML = `<span>${letterToSymbol(letter)} has won!</span>`;
+//   }
+//   setTimeout(handleGameEnd, 1000);
+//   popupTitle.innerHTML = `Game finished,  ${letterToSymbol(letter)} has won!`;
+//   popupReset.addEventListener('click', handlePopupReset)
+//   }
+
+
+
+// const checkGameStatus = () => {                                                           // function of listening all winning combination
+//   const topLeft = cellDivs[0].classList[1];
+//   const topMiddle = cellDivs[1].classList[1];
+//   const topRight = cellDivs[2].classList[1];
+//   const middleLeft = cellDivs[3].classList[1];
+//   const middleMiddle = cellDivs[4].classList[1];
+//   const middleRight = cellDivs[5].classList[1];
+//   const bottomLeft = cellDivs[6].classList[1];
+//   const bottomMiddle = cellDivs[7].classList[1];
+//   const bottomRight = cellDivs[8].classList[1];
+
+//   // check winner                                                                        //Logic of  writing places of all winning combination
+//   if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+//     handleWin(topLeft);
+//     cellDivs[0].classList.add('won');
+//     cellDivs[1].classList.add('won');
+//     cellDivs[2].classList.add('won');
+//   } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+//     handleWin(middleLeft);
+//     cellDivs[3].classList.add('won');
+//     cellDivs[4].classList.add('won');
+//     cellDivs[5].classList.add('won');
+//   } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+//     handleWin(bottomLeft);
+//     cellDivs[6].classList.add('won');
+//     cellDivs[7].classList.add('won');
+//     cellDivs[8].classList.add('won');
+//   } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+//     handleWin(topLeft);
+//     cellDivs[0].classList.add('won');
+//     cellDivs[3].classList.add('won');
+//     cellDivs[6].classList.add('won');
+//   } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+//     handleWin(topMiddle);
+//     cellDivs[1].classList.add('won');
+//     cellDivs[4].classList.add('won');
+//     cellDivs[7].classList.add('won');
+//   } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+//     handleWin(topRight);
+//     cellDivs[2].classList.add('won');
+//     cellDivs[5].classList.add('won');
+//     cellDivs[8].classList.add('won');
+//   } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+//     handleWin(topLeft);
+//     cellDivs[0].classList.add('won');
+//     cellDivs[4].classList.add('won');
+//     cellDivs[8].classList.add('won');
+//   } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+//     handleWin(topRight);
+//     cellDivs[2].classList.add('won');
+//     cellDivs[4].classList.add('won');
+//     cellDivs[6].classList.add('won');
+//   } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {  
+//     gameIsLive = false;
+//     statusDiv.innerHTML = 'Game is tied!';   
+//     statusDiv.style.borderLeft = '4px solid rgb(0, 255, 8)';
+//     statusDiv.style.backgroundColor = 'black'
+//     statusDiv.style.color = 'rgb(0, 255, 8)'
+//   } else {
+//     xIsNext = !xIsNext;
+//     if (xIsNext) {
+//       statusDiv.innerHTML = `${xSymbol} now is your turn`;   
+//       statusDiv.style.color = 'white'
+//       statusDiv.style.borderLeft = '4px solid white';
+//     } else {
+//       statusDiv.innerHTML = `<span>${oSymbol} now is you turn </span>`; 
+//       statusDiv.style.color = 'gold'
+//       statusDiv.style.borderLeft = '4px solid gold';
+//     }
+//   }
+// };
+
+
+// // event Handlers
+// const handleReset = () => {                                             // reset bottom: remove x, 0, won. Bring text of xSymbol is next. Change style of status (text)
+//   xIsNext = true;
+//   statusDiv.innerHTML = `${xSymbol} now is your turn`;   
+//   statusDiv.style.color = 'white'
+//   statusDiv.style.borderLeft = '4px solid white';
+//   for (const cellDiv of cellDivs) {
+//     cellDiv.classList.remove('x');
+//     cellDiv.classList.remove('o');
+//     cellDiv.classList.remove('won');
+//   }
+//   gameIsLive = true;
+// }
+// const handlePopupReset = () => {                                             // reset bottom: remove x, 0, won. Bring text of xSymbol is next. Change style of status (text)
+//   for (const cellDiv of cellDivs) {
+//     cellDiv.classList.remove('x');
+//     cellDiv.classList.remove('o');
+//     cellDiv.classList.remove('won');
+//   }
+//   gameIsLive = true;
+//   document.querySelector('.game-over').style.display = 'none'
+// };
+
+
+
+
+
+
+// const handleCellClick = (e) => {                                       // function that adds x or o to as 2nd class of grid-cells.
+//   const classList = e.target.classList;
+
+//   if (!gameIsLive || classList[1] === 'x' || classList[1] === 'o') {   // x or o. Not both or multiple
+//     return;
+//   }
+
+//   if (xIsNext) {                                                       // first adds x then o then x then o. 
+//     classList.add('x');
+//     checkGameStatus();
+//   } else {
+//     classList.add('o');
+//     checkGameStatus();
+//   }
+// };
+
+
+// // event listeners
+// resetDiv.addEventListener('click', handleReset);                     // while clicking to resetDiv it declares handleReset function
+
+// for (const cellDiv of cellDivs) {                                    // while clicking in grid-cells that are children of grid it declares handleCellClick which adds x or o to element as addition class
+//   cellDiv.addEventListener('click', handleCellClick)
+// }
+
+
+
